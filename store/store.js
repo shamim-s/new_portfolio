18811fbde1626ad2b68d0a4089ab6d7f3ec30415
shamim-s <shamimibas@gmail.com>
2023-08-24 14:01:16 +0600

    Created shop page and product object

---
+++ store/store.js
@@ -0,0 +1,23 @@
+const products = [{
+        id: 1,
+        img: 'https://images.pexels.com/photos/10000/pexels-photo-10000.jpeg?cs=srgb&dl=pexels-photo-10000.jpg&fm=jpg',
+        title: 'Fujifilm GFX 100S Body - Black',
+        des: 'ULTIMATE RESOLUTION, DEPENDABLE PROCESSING: Powered by Fujifilm’s high-performance X-Processor 4 quad-core CPU, GFX100S utilizes its 102MP, back-illuminated large format CMOS sensor to create truly stunning results. Create remarkably detailed still images that possess amazing color fidelity, and rich shadow detail, with enough dynamic range to allow for astonishing push and pull adjustments in post-production. Or, use Pixel Shift Multi-Shot to create ultra-high resolution 400MP stills with remarkable detail. In video mode, record 4K/30p footage in 10-bit F-log or 12-bit ProRes RAW, with an incredibly beautiful depth of field, for a look that is made for the silver-screen.',
+        price: 5999.00,
+        brand: 'Fujifilm',
+        resolution: '102 MP',
+        sensor_size: 'Medium Format (>35mm)',
+
+    },
+    {
+        id: 2,
+        img: 'https://www.ultratech.com.bd/image/cache/catalog/keyboard/razer/cobra/razer-cobra-lightweight-gaming-mouse-2-500x500.jpg.webp',
+        title: 'Razer Cobra Wired Gaming Mouse',
+        des: "58G LIGHTWEIGHT DESIGN — Built to fit most grip styles the mouse allows for fast precise control and feels comfortable to use during long gaming sessions GEN-3 OPTICAL MOUSE SWITCHES — Unrivalled durability and speed with switches that have an extended 90-million click lifecycle and eliminate double-clicking issues boasting a blistering 0.2 ms actuation time without debounce delay CHROMA LIGHTING WITH GRADIENT UNDERGLOW — Customize the mouse with 16.8 million colors and countless lighting effects, creating a more immersive gaming experience as the lights react dynamically with hundreds of Chroma-integrated games PRECISE SENSOR ADJUSTMENTS Fine-tune the mouses sensitivity with precise adjustments in 50 DPI increments to achieve a truly tailored play style",
+        price: 5999.00,
+        brand: 'Fujifilm',
+        resolution: '102 MP',
+        sensor_size: 'Medium Format (>35mm)',
+
+    }
+]
